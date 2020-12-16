e9572efbc1bec73b48669a0e1ed88a599e45bdac
Amir Marcel <amirmarcel@yahoo.com>
2020-12-15 20:07:58 -0500

    created front end components

---
+++ client/Components/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Login from './Login.jsx';
+import SignUp from './SignUp.jsx';
+// import Main from './Main.jsx';
+
+function App() {
+  // creates switchboard
+
+  return (
+      <Router>
+          <Switch>
+              <Route exact path="/">
+                  <Login />
+              </Route>
+              <Route path="/signup">
+                  <SignUp />
+              </Route>
+              {/* <Route path="/main">
+                  <Main />
+              </Route> */}
+          </Switch>
+      </Router>
+  );
+}
+
+export default App;
